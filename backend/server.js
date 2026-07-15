efc736254bb15aa339ceaf62977932f5b8239ca4
prerna <PRERNASONI2000@GMAIL.COM>
2026-07-15 11:49:03 +0530

    mail fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,10 @@ const PORT = Number(process.env.PORT) || 5000
 
 
 //everthing should add after this (connections)
-app.use(cors())
+app.use(cors({
+  origin: process.env.CORS_ORIGIN.split(','),
+  credentials: true
+}))
 app.use('/api/payments/webhook', express.raw({ type: 'application/json' }))
 app.use(express.json({ limit: '50mb' }))
 connectDB();
